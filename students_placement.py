051c3e69f7cb0dbacf63a4c9c65401b3fd0c1fe4
Alberto Casado <albertocasadopeguero@gmail.com>
2020-06-05 17:42:46 -0400

    Update students_placement.py
    Changing the columns names for better undestanding

--- students_placement.py
+++ students_placement.py
@@ -4,3 +4,11 @@ import pandas as pd
 
 #Import the dataset
 dataset = pd.read_csv('Placement_Data.csv')
+
+#Changing columns names
+dataset.columns = ['Serial','Gender','Secondary_Edu','Board_Edu','Higher_Second_Edu',
+                   'Board_Edu_2','Specialization_Higher_Second_Edu','Degree',
+                   'Under_Grad_Degree','Experience','Employability_Test',
+                   'Post_Grad_Specialization','MBA','Placement','Salary']
+
+#EDA
