f944cd9d847538508d0e28f9a1f3746d23b3b571
Alberto Casado <albertocasadopeguero@gmail.com>
2020-06-09 21:21:30 -0400

    Update students_placement.py
    Data encoding

--- students_placement.py
+++ students_placement.py
@@ -2,6 +2,11 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 #Import the dataset
 dataset = pd.read_csv('Placement_Data.csv')
@@ -49,5 +54,33 @@ sns.scatterplot(range(0,len(dataset['employability_test'])),'employability_test'
 sns.scatterplot(range(0,len(dataset['MBA'])),'MBA',
                 hue = 'placement',data = dataset)
 
+#Data correlation
+sns.heatmap(dataset.corr(),annot = True)
+dataset.drop(['secondary_edu'],axis = 1,inplace = True)
+
 #Since the salary has nan values is not a good choice to make a predictor on the salary
-#Since it will to bias towards the placed ones so it became a classificacion problem
+#Since it will be too bias towards the placed ones, theregore it became a classificacion problem
+
+#Checking nan values and dropping more useless columns
+dataset.isnull().sum()
+dataset.drop(['serial','salary'],axis = 1,inplace = True)
+
+#Encoding the categorical values
+label_encoder = LabelEncoder()
+dataset['gender'] = label_encoder.fit_transform(dataset['gender'])
+dataset['board_edu'] = label_encoder.fit_transform(dataset['board_edu'])
+dataset['board_edu_2'] = label_encoder.fit_transform(dataset['board_edu_2'])
+dataset['experience'] = label_encoder.fit_transform(dataset['experience'])
+dataset['MBA_title'] = label_encoder.fit_transform(dataset['MBA_title'])
+dataset['placement'] = label_encoder.fit_transform(dataset['placement'])
+
+columns_to_transform = ['second_edu_speci','degree_title']
+transformer = ColumnTransformer(transformers = [('encoder',OneHotEncoder(),columns_to_transform)],
+                                remainder = 'passthrough')
+X = transformer.fit_transform(dataset.drop(['placement'],axis = 1))
+y = dataset['placement']
+
+#Splitting the dataset
+X_train, X_test, y_train,y_test = train_test_split(X, y,test_size = 0.20)
+
+#Scaling the features
